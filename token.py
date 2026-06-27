8676d6c6754a8c9e36cb8a0451d63dba400b00da
akolkarlahu-sketch <akolkarlahu@gmail.com>
2026-06-27 15:41:44 +0530

    Refactor token.py to use environment variables

--- token.py
+++ token.py
@@ -1,25 +1,20 @@
 import os
 from fyers_apiv3 import fyersModel
 
-
-client_id = os.getenv("FYERS_APP_ID")
-secret_key = os.getenv("FYERS_SECRET_KEY")
-redirect_uri = os.getenv("FYERS_REDIRECT_URI")
-
-
-auth_code = "PASTE_NEW_AUTH_CODE_HERE"
-
+APP_ID = os.getenv("FYERS_APP_ID")
+SECRET_KEY = os.getenv("FYERS_SECRET_KEY")
+REDIRECT_URI = os.getenv("FYERS_REDIRECT_URI")
+AUTH_CODE = os.getenv("FYERS_AUTH_CODE")
 
 session = fyersModel.SessionModel(
-    client_id=client_id,
-    secret_key=secret_key,
-    redirect_uri=redirect_uri,
+    client_id=APP_ID,
+    secret_key=SECRET_KEY,
+    redirect_uri=REDIRECT_URI,
     response_type="code",
     grant_type="authorization_code"
 )
 
-
-session.set_token(auth_code)
+session.set_token(AUTH_CODE)
 
 response = session.generate_token()
 
