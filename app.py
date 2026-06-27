03a84ca1b8c8606a4d53ee508f59049a58818101
akolkarlahu-sketch <akolkarlahu@gmail.com>
2026-06-27 13:54:04 +0530

    Change home message and enhance callback response
    Updated the home route message and modified the callback route to display the received auth code in HTML format.

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "FYERS Bot Server Running"
+    return "Titan AI Signal Bot Running"
+
 
 @app.route("/callback")
 def callback():
-    code = request.args.get("auth_code")
-    print("AUTH CODE:", code)
 
-    return "FYERS Login Successful. You can close this page."
+    auth_code = request.args.get("auth_code")
+
+    print("AUTH CODE RECEIVED:", auth_code)
+
+    return f"""
+    <h2>FYERS Login Successful</h2>
+    <p>Auth Code received:</p>
+    <p>{auth_code}</p>
+    <p>You can close this page.</p>
+    """
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
